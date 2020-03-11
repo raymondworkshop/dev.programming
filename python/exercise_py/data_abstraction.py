c78c0d2174678c35325a35306c0fb477d48d3f64
muyun <wenlzhao@gmail.com>
2020-03-11 22:17:26 +0800

    update

---
+++ python/exercise_py/data_abstraction.py
@@ -0,0 +1,66 @@
+"""
+data abstraction pattern in python  
+
+the part that **operates on abstract data** and the part that **defines a concrete representations** are **connected by a small set of functions**
+ that implement abstract data in terms of the concrete representation.  
+
+Note: the underlying idea of data abstraction is to **identify a basic set of operations** in terms of which all manipulations of values 
+of some kind will be expressed, and then to **use only those operations in manipulating the data**. By **restricting the use of operations in
+this way**, it is much **easier to change the representation of abstract data** without changing the behavior of a program.  
+
+We can express abstract data using a collection of selectors and constructors, together
+with some behavior conditons.  
+
+As long as the behavior conditions are met, the selectors and constructors constitute a valide repreesentation of a kind of data.  
+
+The implementation details below an abstraction barrier may change, but **if the behavior does not change**, then the data abstraction remains valid,
+and any program written using this data abstraction will remain correct.  
+"""
+
+# 1. use rational numbers to perform computation  like add, mul, equal, print  
+# operates on abstract data  
+def add_rational(x, y):
+    """
+    x = nx / dx, y = ny / dy
+    """
+    return
+
+def mul_rational():
+    return
+
+def rationals_are_equal(x, y):
+    return
+
+def print_rational(x):
+    print(numer(x), '/', denom(x))
+    return
+
+# 2. represent rational numbers  
+# create rationals or implement rational operations  
+# a rational number as a pair of two integers: a numerator and a denominator  
+# r = n / d  
+# **define operations in terms of a constructor rational(), and selectors numer and denom** in the example   
+# 
+def rational(n, d):
+    """
+    return the rational number with numerator n and denominator d
+    """
+    return [n, d] # the implementation
+
+def numer(x):
+    """
+    return the numerator of the rational number x
+    """
+    return x[0]
+
+def denom(x):
+    """
+    return the denominator of the rational number x
+    """
+    return x[1]
+
+
+
+if __name__ == "__main__":
+    half = rational(1, 2)
+    print_rational(half)
