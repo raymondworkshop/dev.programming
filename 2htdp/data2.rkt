f359f8977463bb6b460d0c0c00aa4d992b7f1b8f
muyun <wenlzhao@gmail.com>
2021-08-04 21:05:48 +0800

    add

--- 2htdp/data2.rkt
+++ 2htdp/data2.rkt
@@ -1,8 +1,14 @@
 #lang racket
-; basic data: numbers, strings, images, boolean values
-; enumerations, intervals 
-;
+; 1. basic data: numbers, strings, images, boolean values
+; 2. ranges of data: enumerations, intervals 
 ; an enumeration defines **a collection of data** as a finite number of pieces of data ;
+; 3. structure-based data
+;
+; **data definition** is to describe parts of the universe
+; and to name these parts so that we can refer to them concisely
 ; 
-; TODO - 4.4 Intervals  
+; TODO - 5.8  - design recipe
+;  1. data definitions with examples
+;  
+;  2. functions with behavior example
 ;
