21e6e278687c3029a8e650b28977b0ec3c1805b6
muyun <wenlzhao@gmail.com>
2021-09-07 18:01:34 +0800

    update

--- 2htdp/list.rkt
+++ 2htdp/list.rkt
@@ -1,3 +1,46 @@
 #lang racket
 ;
-; TODO - 10 More on Lists
+(require 2htdp/batch-io)
+; TODO - 10.3 Lists in Lists, Files
+;
+;  Data representation  
+;
+;   A list of string (los) is -
+;    - '()
+;    - (cons String los)
+(cons "TTT" '())
+(cons "" (cons "TTT" '()))
+(cons "Put up in a place" (cons "" (cons "TTT" '())))
+(read-file "ttt.txt")
+; 
+;  A list of words (low) is -
+;    - '()
+;    - (cons String lows)
+(cons "TTT" '())
+(cons "Put" (cons "TTT" '()))
+(read-words "ttt.txt")
+;
+;  A list of list of strings (lls) is - 
+;    - '() 
+;    - (cons String lls) 
+(cons "TTT" '())
+(cons "" (cons "TTT" '()))
+(cons "Put" (cons "" (cons "TTT" '())))
+(cons "up" (cons "Put" (cons "" (cons "TTT" '()))))
+(read-words/line "ttt.txt")
+;
+;
+; how many words appear per line 
+; 
+; list of lists of strings (lls) -> list-of-numbers  
+; determines the number of words on each line 
+; data examples
+(define line0 '())
+;(define line1 (cons "world" '()))
+(define line2 (cons "hello" (cons "world" '())))
+
+(define lls0 '())
+;(define lls1 )
+
+;  the head of desired function
+(define (words-on-line lls) '())
