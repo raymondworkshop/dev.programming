7e988325b81ac90cdc55ea46a9d094b53116832f
muyun <wenlzhao@gmail.com>
2022-02-12 17:21:51 +0800

    update

---
+++ python/fluent/vector2d.py
@@ -0,0 +1,80 @@
+"""
+ch11 - a pythonic Object
+"""
+# vector2d.py
+
+import math
+
+
+class Vector2d:
+    #list the instance attributes in the order 
+    # they will be used for positional pattern matching 
+    __match_args__ = ('x', 'y') 
+
+    def __init__(self, x, y):
+        self.x = float(x)
+        self.y = float(y)
+
+    def __repr__(self):
+        return f"Vector({self.x!r}, {self.y!r})"
+
+    def __str__(self):
+        return str(tuple(self))
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector2d(x, y)
+
+    def __mul__(self, scalar):
+        return Vector2d(self.x * scalar, self.y * scalar)
+
+    def __abs__(self):
+        return math.hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def __iter__(self):  # make unpacking work
+        return (i for i in (self.x, self.y))
+
+    def __eq__(self, other):
+        return tuple(self) == tuple(other)
+
+    def __format__(self, fmt_spec=""):  # format
+        components = (format(c, fmt_spec) for c in self)
+        return "({}, {})".format(*components)
+
+    def __hash__(self):  # hashable
+        return hash(self.x) ^ hash(self.y)
+
+
+def test_vector():
+    v1 = Vector2d(2, 4)
+    v2 = Vector2d(2, 1)
+    print(v1 + v2)
+
+    v = Vector2d(3, 4)
+    print(v * 3)
+
+    print(abs(v))
+
+
+def test_keyword_pattern(v: Vector2d) -> None:
+    match v:
+        case Vector2d(x=0, y=0):
+            print(f'{v!r} is null')
+        case Vector2d(0,_):
+            print(f'{v!r} is vertical')
+        case Vector2d(_, 0):
+            print(f'{v!r} is horizontal')
+        case _:
+            print(f'{v!r} is awesome')
+    
+
+
+if __name__ == "__main__":
+    test_vector()
+    #
+    test_keyword_pattern(Vector2d(2, 0))
+    #
