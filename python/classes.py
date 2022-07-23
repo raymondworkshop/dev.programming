f8ce9944a655408d9d7b825dd65eec4648506850
muyun <wenlzhao@gmail.com>
2022-07-23 16:31:28 +0800

    update

---
+++ python/classes.py
@@ -0,0 +1,15 @@
+"""
+ch7 - classes and object-oriented programming in <Python Distilled>
+"""
+
+
+def object():
+    return
+
+
+def main():
+    object()
+
+
+if __name__ == "__main__":
+    main()
