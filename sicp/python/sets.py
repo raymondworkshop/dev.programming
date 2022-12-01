75948af3b5e5498fa0916e25f6be75771e356698
muyun <wenlzhao@gmail.com>
2022-12-01 17:07:10 +0800

    update

--- sicp/python/sets.py
+++ sicp/python/sets.py
@@ -1,9 +1,27 @@
-"""
-2.9.3 Sets
-"""
+#2.9.3 Sets
+
+
+from link import Link
+
+# sets as unordered sequences  
+# a set is as a sequence in which no element appears more than ones
+# and the empty set is represented by the empty sequence 
+
+def set_contains(s, v):
+    #"""return True if and only if set s contains v."""
+    if s is Link.empty:
+        return False
+    elif s.first == v:
+        return True
+    else:
+        return set_contains(s.rest, v)
+
+#
 
 def main():
-    pass
+    s = Link(3, Link(4, Link(5)))
+    print(set_contains(s, 2))
+    
 
 if __name__ == "__main__":
     main()
